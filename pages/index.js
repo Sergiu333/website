96de4668364ed9a107b9c1916265d2438a690851
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-13 18:37:28 +0300

    feat: created ensome

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import Ensome from "../components/ensome";
 
 const Home = () =>{
     return (
         <div>
             <Header/>
+            <Ensome/>
         </div>
     )
 }
