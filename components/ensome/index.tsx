556ffcedb39a22256c570ef38020744c51d61645
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-18 00:35:31 +0300

    feat: edited the code

--- components/ensome/index.tsx
+++ components/ensome/index.tsx
@@ -1,6 +1,6 @@
 const Ensome = () => {
   return (
-      <div className="container mx-auto px-[202px] mt-[70px] flex flex-row gap-[220px]">
+      <div className="container mx-auto w-[1110px] mt-[70px] flex flex-row gap-[220px]">
           <div className="text-[#292D33] text-[46px] font-bold leading-[60px]">Find true power in your <br/> data with <span className="text-[#185CFF]">Ensome</span></div>
           <div className="text-base text-[#9497A1] w-[350px] leading-7">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</div>
       </div>
