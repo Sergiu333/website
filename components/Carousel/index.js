1da09fdb5a038739d4a1872ffd94298fd12d0c6b
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-19 23:55:18 +0300

    feat: edited the carousel

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -9,112 +9,95 @@ const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
     ssr: false,
 });
 
-
+const testimonials=[
+    {
+        src: "/person_1.jpg",
+        name: "Alex Bern",
+        ceo: "CEO by PixelPerfect",
+        description: "“Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse quam nihil molesti consequatur, vel illum.” "
+    },
+    {
+        src: "/person_2.jpg",
+        name: "Ruben Chifundo",
+        ceo: "CEO by NOX",
+        description: "“Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse quam nihil molesti consequatur, vel illum.” "
+    },
+    {
+        src: "/person_3.jpg",
+        name: "Tigran Nazaret",
+        ceo: "Data analyst",
+        description: "“Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse quam nihil molesti consequatur, vel illum.” "
+    }
+]
 
 const Carousel = () => {
     return (
-        <div className="container mx-auto w-[1110px] flex flex-row gap-[30px] mt-[120px]">
-            <OwlCarousel
-                autoplay
-                infinite={true}
-                loop={true}
-                dots
-                nav={true}
-                // navText={["<img src='/Create.png'>","<img src='/Create.png'>"]}
-                // navText= {["<div class='owl-prev'>back</div>", "<div class='owl-next'>next</div>"]}
-                navText= {[
-                    "<div class='back'>back</div>",
-                    "<div class='next'>next</div>"
-                ]}
+        <div className="container mx-auto w-[1110px] mt-[120px] mb-[100px]">
+            <div className="text-[#292D33] Headline_2_ExtraBold mb-[50px]">Testimonials </div>
+            <div className=" flex flex-row gap-[30x]">
+                <OwlCarousel
+                    autoplay
+                    loop={true}
+                    dots
+                    nav={true}
+                    navText = {[
+                        "<div class='back'>back</div>",
+                        "<div class='next'>next</div>"
+                    ]}
+                >
+                    {testimonials.map(({src, alt, name,ceo,  description}) => {
+                        return (
+                            <div className="  p-[10px]">
+                                <div className="flex flex-col p-[35px] shadow-card-3 ml-[30px]">
+                                    <div className="flex flex-row gap-[25px]">
+                                        <div>
+                                            <Image
+                                                src={src}
+                                                alt="person"
+                                                width={80}
+                                                height={80}
+                                            />
+                                        </div>
+                                        <div className="flex flex-col gap-1">
+                                            <div className="Headline_6_Bold text-[#292D33]">{name}</div>
+                                            <div className="Headline_7_Medium text-[#9497A1]">{ceo}</div>
+                                        </div>
+                                    </div>
+                                    <div className="Paragraph_2_Regular text-[#9497A1]">
+                                        {description}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
 
-            >
-                <div className="w-[350px] p-[35px] flex flex-col shadow-card-3">
-                    <div className="flex flex-row gap-[25px]">
-                        <div>
-                            <Image
-                                src="/person_1.jpg"
-                                alt="Person"
-                                width={80}
-                                height={80}
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <div className="Headline_6_Bold text-[#292D33]">Alex Bern</div>
-                            <div className="Headline_7_Medium text-[#9497A1]">CEO by PixelPerfect</div>
-                        </div>
-                    </div>
-                    <div className="Paragraph_2_Regular text-[#9497A1]">
-                        Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut
-                        aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse
-                        quam nihil molesti consequatur, vel illum.
-                    </div>
-                </div>
-                <div className="w-[350px] p-[35px] flex flex-col shadow-card-3">
-                    <div className="flex flex-row gap-[25px]">
-                        <div>
-                            <Image
-                                src="/person_1.jpg"
-                                alt="Person"
-                                width={80}
-                                height={80}
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <div className="Headline_6_Bold text-[#292D33]">Alex Bern</div>
-                            <div className="Headline_7_Medium text-[#9497A1]">CEO by PixelPerfect</div>
-                        </div>
-                    </div>
-                    <div className="Paragraph_2_Regular text-[#9497A1]">
-                        Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut
-                        aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse
-                        quam nihil molesti consequatur, vel illum.
-                    </div>
-                </div>
-                <div className="w-[350px] p-[35px] flex flex-col shadow-card-3">
-                    <div className="flex flex-row gap-[25px]">
-                        <div>
-                            <Image
-                                src="/person_1.jpg"
-                                alt="Person"
-                                width={80}
-                                height={80}
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <div className="Headline_6_Bold text-[#292D33]">Alex Bern</div>
-                            <div className="Headline_7_Medium text-[#9497A1]">CEO by PixelPerfect</div>
-                        </div>
-                    </div>
-                    <div className="Paragraph_2_Regular text-[#9497A1]">
-                        Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut
-                        aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse
-                        quam nihil molesti consequatur, vel illum.
-                    </div>
-                </div>
-                <div className="w-[350px] p-[35px] flex flex-col shadow-card-3">
-                    <div className="flex flex-row gap-[25px]">
-                        <div>
-                            <Image
-                                src="/person_1.jpg"
-                                alt="Person"
-                                width={80}
-                                height={80}
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <div className="Headline_6_Bold text-[#292D33]">Alex Bern</div>
-                            <div className="Headline_7_Medium text-[#9497A1]">CEO by PixelPerfect</div>
-                        </div>
-                    </div>
-                    <div className="Paragraph_2_Regular text-[#9497A1]">
-                        Ut enim ad minima veniam, quis nostrum exercitationem ullam corpor suscipit laboriosam, nisi ut
-                        aliquid ex ea commodi consequatur? Quis utem vel eum iure reprehender qui in ea voluptate velit esse
-                        quam nihil molesti consequatur, vel illum.
-                    </div>
-                </div>
-
-            </OwlCarousel>
-
+                    {testimonials.map(({src, alt, name,ceo,  description}) => {
+                        return (
+                            <div className="  p-[10px]">
+                                <div className="flex flex-col p-[35px] shadow-card-3 ml-[30px]">
+                                    <div className="flex flex-row gap-[25px]">
+                                        <div>
+                                            <Image
+                                                src={src}
+                                                alt="person"
+                                                width={80}
+                                                height={80}
+                                            />
+                                        </div>
+                                        <div className="flex flex-col gap-1">
+                                            <div className="Headline_6_Bold text-[#292D33]">{name}</div>
+                                            <div className="Headline_7_Medium text-[#9497A1]">{ceo}</div>
+                                        </div>
+                                    </div>
+                                    <div className="Paragraph_2_Regular text-[#9497A1]">
+                                        {description}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </OwlCarousel>
+            </div>
         </div>
     )
 }
