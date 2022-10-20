76861e77d8d196a1f62b89d7e97056f2451ecfa1
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-21 01:21:18 +0300

    feat: added link for button from contact component

--- components/contacts/index.tsx
+++ components/contacts/index.tsx
@@ -1,4 +1,6 @@
-const Contacts = () =>{
+import Link from "next/link";
+
+const Contacts = () => {
     return (
         <div className="bg-[#F1F6FA]">
             <div className="container mx-auto w-[1110px] flex flex-col gap-[30px] py-[100px] items-center">
@@ -6,11 +8,16 @@ const Contacts = () =>{
                     Do you need help?
                 </div>
                 <div className="Paragraph_2_Regular text-gray">
-                    Ut enim ad minima veniam, quis nostrum exercitationem ullam <br/> corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.
+                    Ut enim ad minima veniam, quis nostrum exercitationem ullam <br/> corporis suscipit laboriosam, nisi
+                    ut aliquid ex ea commodi.
                 </div>
-                <div className="bg-primary text-white py-[15px] px-[52px] w-[190px] rounded-md">
-                    Contact Us
-                </div>
+                <Link href="/">
+                    <a>
+                        <div className="bg-primary text-white py-[15px] px-[52px] w-[190px] rounded-md">
+                            Contact Us
+                        </div>
+                    </a>
+                </Link>
             </div>
         </div>
     )
