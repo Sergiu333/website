59052bf09da0b36b88f820d21487788c0a2128d7
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-26 00:59:15 +0300

    feat: created services component

---
+++ pages/overviewSection/index.tsx
@@ -0,0 +1,30 @@
+const OverviewSection = () => {
+    return (
+        <div className="container mx-auto w-[1110px] text-center flex flex-col">
+            {/*<div className="text-black Headline_2_ExtraBold">We provide servicesCompany that guarantee your success</div>*/}
+            {/*<div className="flex flex-row">*/}
+            {/*    <div className="flex flex-row gap-[30px]">*/}
+            {/*        <div className="flex flex-col">*/}
+            {/*            <div className="text-primary ">1830+</div>*/}
+            {/*            <div>Project executed</div>*/}
+            {/*        </div>*/}
+            {/*        <div className="flex flex-col">*/}
+            {/*            <div className="text-primary ">834+</div>*/}
+            {/*            <div>Satisfied customers</div>*/}
+            {/*        </div>*/}
+            {/*        <div className="flex flex-col">*/}
+            {/*            <div className="text-primary ">390</div>*/}
+            {/*            <div>Data management</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="text-gray Paragraph_1_Regular">*/}
+            {/*        Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium,*/}
+            {/*        totam rem aperiam, eaque ipsa quaeab illo inventore. Donec tincidunt tempor quam, non mollis quam*/}
+            {/*        finibus nec.*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
+    );
+}
+
+export default OverviewSection;
