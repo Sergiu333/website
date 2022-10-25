bc66180d4d23932e5d89d77cfc7d10236e90dba0
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-25 23:55:05 +0300

    feat: created breadcrumd component

---
+++ pages/Services/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import Breadcrumd from "../../components/Breadcrumd";
+
+const Services = () => {
+    return (
+        <div>
+            <Header/>
+            <Breadcrumd/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Services;
