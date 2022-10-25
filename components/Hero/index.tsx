008a219057f407eae15c88895f57570d876fb729
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-26 00:04:38 +0300

    feat: created hero component

---
+++ components/Hero/index.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+    return (
+        <div className="container mx-auto w-[1110px] flex flex-col text-center pt-14 pb-[120px]">
+            <div className="text-primary Headline_6_Bold">
+                Services
+            </div>
+            <div className="text-black Headline_1_ExtraBold">
+                Data Analytics Services
+            </div>
+            <div className="text-gray Paragraph_1_Regular">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium <br/>doloremque laudantium, totam
+                rem aperiam, eaque ipsa quaeab illo inventore.
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
