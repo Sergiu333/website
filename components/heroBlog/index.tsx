74470a77831cb7e8c5df727e91997237983a8651
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-26 16:39:32 +0300

    feat: created heroBlog component

---
+++ components/heroBlog/index.tsx
@@ -0,0 +1,17 @@
+const HeroBlog = () => {
+    return (
+        <div className="container mx-auto w-[1110px] flex flex-col text-center pt-14">
+            <div className="text-primary Headline_6_Bold">
+                Blog
+            </div>
+            <div className="text-black Headline_1_ExtraBold">
+                Discover new things with Ensome blog
+            </div>
+            <div className="text-gray Paragraph_1_Regular">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium <br/> doloremque laudantium, totam rem aperiam, eaque ipsa quaeab illo inventore.
+            </div>
+        </div>
+    );
+}
+
+export default HeroBlog;
---
+++ pages/Blog/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/header";
+import Breadcrumd from "../../components/Breadcrumd";
+import HeroBlog from "../../components/heroBlog";
+
+const Blog = () =>{
+    return (
+        <div className="container mx-auto w-[1110px]">
+           <Header/>
+            <Breadcrumd/>
+            <HeroBlog/>
+        </div>
+    );
+}
+
+export default Blog;
