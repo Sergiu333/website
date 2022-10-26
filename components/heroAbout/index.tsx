bdddf4c61cf243ebf48083e3efd74034ca81715e
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-26 11:16:35 +0300

    feat: created heroAboutUs component

---
+++ components/heroAbout/index.tsx
@@ -0,0 +1,17 @@
+const HeroAbout = () => {
+  return (
+      <div className="container mx-auto w-[1110px] flex flex-col text-center pt-14 gap-[15px]">
+          <div className="text-primary Headline_6_Bold">
+              About Us
+          </div>
+          <div className="text-black Headline_1_ExtraBold">
+              We work with 15 years of experience
+          </div>
+          <div className="text-gray Paragraph_1_Regular">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium <br/> doloremque laudantium, totam rem aperiam, eaque ipsa quaeab illo inventore.
+          </div>
+      </div>
+  );
+}
+
+export default HeroAbout;
