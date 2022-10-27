5c65b134e7be9365ca71c6744cd7a27680f87932
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-27 10:42:57 +0300

    feat: create form component for contacts page

--- pages/Contacts/index.tsx
+++ pages/Contacts/index.tsx
@@ -1,12 +1,14 @@
 import Header from "../../components/header";
 import Breadcrumd from "../../components/Breadcrumd";
 import Footer from "../../components/footer";
+import Form from "../../components/Form";
 
 const Contacts = () => {
   return(
       <div>
           <Header/>
           <Breadcrumd/>
+          <Form/>
           <Footer/>
       </div>
   );
