f6dbb6bcb4677836bc7f623b0ff41f5dee43a5cf
Sergiu <sergiu.anghelenici@corlab.tech>
2022-10-27 09:30:10 +0300

    feat: added footer

--- pages/Contacts/index.tsx
+++ pages/Contacts/index.tsx
@@ -1,12 +1,13 @@
 import Header from "../../components/header";
 import Breadcrumd from "../../components/Breadcrumd";
+import Footer from "../../components/footer";
 
 const Contacts = () => {
   return(
       <div>
           <Header/>
           <Breadcrumd/>
-
+          <Footer/>
       </div>
   );
 }
